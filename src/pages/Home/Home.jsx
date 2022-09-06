378aae8ee9e1e89c167120f0525193ab6fc5d1fd
Onyedikachi123 <onyedikachierugo@gmail.com>
2022-09-06 01:31:39 +0100

    second commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+import Header from '../../components/header/header'
+import Featured from '../../components/Featured/featured'
+import PropertyList from '../../components/PropertyList/PropertyList'
+import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties'
+import MailList from '../../components/MailList/MailList'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Home;
